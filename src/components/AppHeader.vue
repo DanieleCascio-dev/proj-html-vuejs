a047b35aff6347f6ed18e5be56a736c35f759355
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-12-04 14:25:15 +0100

    add. AppNav (wip)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,19 @@
 <script>
-export default {};
+import AppNav from "./AppNav.vue";
+
+export default { components: { AppNav } };
 </script>
 
 <template>
   <h2>HEADER</h2>
+
+  <!-- Nav -->
+  <AppNav />
+  <!-- /Nav -->
+
+  <!-- Hero -->
+  <h3>Hero</h3>
+  <!-- /Hero -->
 </template>
 
 <style scoped lang="scss">
---
+++ src/components/AppNav.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  data() {
+    return {
+      displayMenu: false,
+    };
+  },
+  methods: {
+    showMenu() {
+      this.displayMenu = !this.displayMenu;
+    },
+  },
+};
+</script>
+
+<template>
+  <nav>
+    <div class="logo">
+      <img src="../assets/img/avada-music-logo.png" alt="" />
+    </div>
+
+    <div class="nav-menu">
+      <i class="fa-solid fa-bars" @click="showMenu"></i>
+
+      <ul class="nav-links" v-show="displayMenu">
+        <li>Link</li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+@use "../style/partials/mixin" as *;
+
+nav {
+  /* DEBUG */
+  border: 1px dashed black;
+  background-color: darkgray;
+
+  height: 80px;
+  width: 100%;
+  @include flex(row, space-between, center);
+  padding: 0 2rem;
+
+  .logo {
+    width: 150px;
+  }
+
+  .nav-menu {
+    width: 200px;
+    @include flex(row, center, center);
+    cursor: pointer;
+
+    .nav-links {
+      @include flex(row, center, center);
+    }
+  }
+}
+</style>
